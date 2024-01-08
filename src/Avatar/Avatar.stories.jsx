da3d0d4d84e085077a7e5b2491766cba4ef12040
Piotr Kulpinski <piotr@kulpinski.pl>
2024-01-08 15:39:46 +0100

    Update package name and gitignore

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -3,6 +3,10 @@ import { Avatar } from "./Avatar"
 export default {
   title: "Design System/Avatar",
   component: Avatar,
+  tags: ["autodocs"],
+  parameters: {
+    componentSubtitle: "Displays an image that represents a user or organization",
+  },
 }
 
 export const Standard = {
