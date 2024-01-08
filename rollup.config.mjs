6e811b9f7ac160a0d8e7eb152f1b01203a0b3e23
Piotr Kulpinski <piotr@kulpinski.pl>
2024-01-08 17:14:04 +0100

    Update Rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,44 +1,45 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { babel } from '@rollup/plugin-babel';
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import terser from "@rollup/plugin-terser"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import { babel } from "@rollup/plugin-babel"
 
 // This is required to read package.json file when
 // using Native ES modules in Node.js
 // https://rollupjs.org/command-line-interface/#importing-package-json
-import { createRequire } from 'node:module';
-const requireFile = createRequire(import.meta.url);
-const packageJson = requireFile('./package.json');
+import { createRequire } from "node:module"
+const requireFile = createRequire(import.meta.url)
+const packageJson = requireFile("./package.json")
 
 export default [
   {
-    input: 'src/index.js',
+    input: "src/index.js",
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
+        format: "cjs",
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: 'esm',
-        exports: 'named',
+        format: "esm",
+        exports: "named",
         sourcemap: true,
       },
     ],
     plugins: [
       peerDepsExternal(),
       resolve({
-        extensions: ['.js', '.jsx'],
+        extensions: [".js", ".jsx"],
+        preferBuiltins: true,
       }),
       commonjs(),
       terser(),
       babel({
-        extensions: ['.js', '.jsx'],
-        exclude: 'node_modules/**',
+        extensions: [".js", ".jsx"],
+        exclude: "node_modules/**",
       }),
     ],
-    external: ['react', 'react-dom', '@emotion/react', '@emotion/styled'],
+    external: ["react", "react-dom", "@emotion/react", "@emotion/styled"],
   },
-];
+]
