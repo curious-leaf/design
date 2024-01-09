06be6454988eaed09b6400f6059e8dfbf0ebb38f
Piotr Kulpinski <piotr@kulpinski.pl>
2024-01-09 12:52:08 +0100

    Update CI

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
 import commonjs from "@rollup/plugin-commonjs"
+import terser from "@rollup/plugin-terser"
 import peerDepsExternal from "rollup-plugin-peer-deps-external"
 import { dts } from "rollup-plugin-dts"
 
@@ -27,7 +28,7 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), terser(), typescript()],
     external: ["react", "react-dom"],
   },
   {
